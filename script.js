72f1684a09cd6d2cfdf47387ca3072804cf98b0c
Marie Malarme <malarmemarie@gmail.com>
2019-03-11 01:53:32 +0000

    Set up of the rotating object.

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('mousemove', event => {
+  const x = event.pageX
+  const y = event.pageY
+  const midX = x - window.innerWidth / 2
+  const midY = y - window.innerHeight / 2
+  const box = document.querySelector('section')
+  const faces = Array.from(document.querySelectorAll('.face'))
+  box.style.transform = `rotateX(${midY / 2}deg) rotateY(${midX}deg)`
+  faces.map(face => (face.style.backgroundColor = `hsl(${midX / 4}, 70%, 65%)`))
+})
