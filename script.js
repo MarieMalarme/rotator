8dc551be02a20a0a553ce2236cc6db18a7fd7b8b
Marie Malarme <malarmemarie@gmail.com>
2019-03-13 02:45:24 +0000

    Added an under layer section with text (poem from Stéphane Mallarmé).

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('mousemove', event => {
   const y = event.pageY
   const midX = x - window.innerWidth / 2
   const midY = y - window.innerHeight / 2
-  const box = document.querySelector('section')
+  const box = document.querySelector('.cube')
   const faces = Array.from(document.querySelectorAll('.face'))
   box.style.transform = `rotateX(${midY / 2}deg) rotateY(${midX}deg)`
   faces.map(face => (face.style.backgroundColor = `hsl(${midX / 4}, 70%, 65%)`))
